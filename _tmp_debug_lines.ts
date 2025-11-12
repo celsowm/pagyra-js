969b8d30159ba07aeea22e74f705c9d7e0b540dc
celsowm <celsowm@gmail.com>
2025-11-12 11:52:33 -0300

    just cleaning

--- _tmp_debug_lines.ts
+++
@@ -1,8 +0,0 @@
-import { prepareHtmlRender } from './src/html-to-pdf.js';
-const html = await (await import('fs/promises')).readFile('playground/public/examples/justify-text.html', 'utf8');
-const { layoutRoot } = prepareHtmlRender({ html, css: '', viewportWidth: 800, viewportHeight: 1000, pageWidth: 800, pageHeight: 1000, margins: { top: 0, right: 0, bottom: 0, left: 0 } });
-layoutRoot.walk(node => {
-  if (node.lineBoxes) {
-    console.log(node.lineBoxes.map(l => ({ text: l.text, width: l.width, spaceCount: l.spaceCount, target: l.targetWidth })));
-  }
-});
