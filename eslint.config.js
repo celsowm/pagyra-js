775a5f0892af1f1073abcce732e4326d817e99ec
celsowm <celsowm@gmail.com>
2025-11-12 23:25:21 -0300

    chore: disable no-undef ESLint rule
    Disable the no-undef rule in the ESLint configuration to prevent warnings on undefined variables, likely due to TypeScript handling or global definitions.

--- eslint.config.js
+++ eslint.config.js
@@ -30,6 +30,7 @@ export default [
       ...tseslint.configs.recommended.rules,
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/no-explicit-any': 'warn',
+      'no-undef': 'off',
     },
   },
 ];
